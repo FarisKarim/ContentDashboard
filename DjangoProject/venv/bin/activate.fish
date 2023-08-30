d0215bc2389d03b4083076158fef0491a612bc81
Faris <fariskarim2000@gmail.com>
2023-08-30 17:21:31 -0400

    fixpaths

--- DjangoProject/venv/bin/activate.fish
+++ DjangoProject/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/faris/Desktop/DjangoProject/venv"
+set -gx VIRTUAL_ENV "/Users/faris/Desktop/ProjectDashboard/DjangoProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
