91dfd0855ddff745a597e217781db1a42eb4fc66
Faris <fariskarim2000@gmail.com>
2023-08-31 16:20:43 -0400

    company and job opening components and routes

--- frontend_dashboard/src/App.js
+++ frontend_dashboard/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import NotFound from './components/NotFound/NotFound';
-import Signup from './components/Signup/Signup';
-import ProtectedRoute from './utils/ProtectedRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
+import Signup from "./components/Signup/Signup";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import CompanyList from "./components/Company/CompanyList";
+import JobOpeningList from "./components/JobOpenings/JobOpeningsList";
 
 const App = () => {
   return (
@@ -13,13 +15,18 @@ const App = () => {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<ProtectedRoute/>}>
-            <Route index element={<Home />} /> 
+          <Route path="/home" element={<ProtectedRoute />}>
+            <Route index element={<Home />} />
           </Route>
+          <Route path="/companies" element={<CompanyList />} />
+          <Route
+            path="/companies/:id/job_openings"
+            element={<JobOpeningList />}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 export default App;
