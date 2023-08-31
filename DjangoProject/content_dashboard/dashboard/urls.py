c386a71da4d877f83df51d95844dfdcbac0cb084
Faris <fariskarim2000@gmail.com>
2023-08-30 20:51:57 -0400

    authentication

--- DjangoProject/content_dashboard/dashboard/urls.py
+++ DjangoProject/content_dashboard/dashboard/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, SomeEndpointView, CustomAuthToken
+
 
 urlpatterns = [
     path('api/register/', UserRegistrationView.as_view(), name='user-register'),
+    path('api/some_endpoint/', SomeEndpointView.as_view(), name='some-endpoint'),
+    path('api/login/', CustomAuthToken.as_view()),
 ]
--- DjangoProject/content_dashboard/dashboard/views.py
+++ DjangoProject/content_dashboard/dashboard/views.py
@@ -1,8 +1,12 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import CustomUser
 from .serializers import UserRegistrationSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
 
 class UserRegistrationView(APIView):
     def post(self, request):
@@ -13,4 +17,16 @@ class UserRegistrationView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class SomeEndpointView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def get(self, request):
+        return Response({"message": "Hello, " + request.user.first_name + "!"})
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
